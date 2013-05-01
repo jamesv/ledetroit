652d08b0a4b23691dd134711fac721169842d3a8
jamesv <j@mesv.org>
2013-05-01 17:11:48 -0400

    tacos

---
+++ 2013/05/life/tacos.php
@@ -0,0 +1,307 @@
+<?php
+    $page = array(
+        'id' => '2013_05_life_tacos',
+        'title' => 'The Atacolypse',
+        'class' => 'life',
+        'content_css' => true
+    );
+
+    $doc_root = $_SERVER['DOCUMENT_ROOT'];
+    include_once($doc_root . '/includes/header.php'); 
+    include_once($doc_root . '/includes/functions.php'); 
+?>
+    <!-- Containers -->
+    <div class='cols ac intro gutter_top'>
+
+        <div class='title'><h1>The<br/>Atacolypse</h1></div>
+        <h4 class='col col_10 col_offset_1 last'>A Taco Tour with Solid Dudes Kitchen</h4>
+        <div class='ac'><p class='col col_6 col_offset_3 last'>Cinco De Mayo is upon us, and in honor of it, we asked our favorite food fans, Dave Graw and Derek Swanson of Solid Dudes Kitchen, to taste test some of the best tacos in town. They complied with delight, and promptly mapped out the five-stop taco tour. Unbeknownst to us, they had set a goal for themselves: 10 tacos each. Unbeknownst to them, they had to get it all done in a little over four hours. <nobr>Hilarity—and pain</nobr>, lots of pain—ensues.</p></div>
+        <div class='photographer'>Chris Arace</div> 
+    </div>
+    
+    
+
+    <!-- Barzon -->
+    <div id='barzon' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>El Barzon</h2>
+        </div>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/05/life/tacos_barzon_00.jpg' />
+        </div>
+        <div class='col col_12 c last'>
+            <p class='big order'>THE TACOS: Carne Enchilada, Chorizo, Barbacoa de Cabra</p>
+        </div>
+
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Carne Enchilada</p>
+            <p class='big'>Dave: I’m just saying, this is probably gonna be the best one of the day.</p>
+            <p class='big'>Derek: This is muy caliente. This is the best one I’ve had all day. Also the first.</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_barzon_01.jpg' />
+        </div>
+
+        <div class='col col_8 row'>
+            <img src='/img/content/2013/05/life/tacos_barzon_02.jpg' />
+        </div>
+        <div class='col col_4 row last'>
+            <p class='big u'>Chorizo</p>
+            <p class='big'>Derek: Shit. Fuck. It’s super light. Has little burnt crackles.</p>
+        </div>
+
+        <div class='col col_4 row_l'>
+            <p class='big u'>Barbacoa de Cabra</p>
+            <p class='big'>Dave: DUDE.</p>
+            <p class='big'>Derek: Fattier than I’d thought. Tastefully fatty.</p>
+        </div>
+        <div class='col col_8 row_l last'>
+            <img src='/img/content/2013/05/life/tacos_barzon_03.jpg' />
+        </div>
+    </div>
+    <div class='callout_row'>
+        <strong>El Barzon</strong> 3710 Junction St. Detroit, MI 48210
+    </div>
+    </div>
+    
+    
+    
+    <!-- Pueblo -->
+    <div id='pueblo' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>Taqueria Mi Pueblo</h2>
+        </div>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/05/life/tacos_pueblo_00.jpg' />
+        </div>
+        <div class='col col_12 c last'>
+            <p class='big order'>THE TACOS: Lengua (beef tongue), Cabeza (beef head),<br/>
+            Tinga (Spicy chicken breast), Chicharrones (pork rinds)</p>
+        </div>
+
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Lengua</p>
+            <p class='big'>Derek: I just French kissed a cow. I can see the taste buds.</p>
+            <p class='big'>Dave: It’s real similar [to Cabeza]</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_pueblo_01.jpg' />
+        </div>
+
+        <div class='col col_8 row'>
+            <img src='/img/content/2013/05/life/tacos_pueblo_02.jpg' />
+        </div>
+        <div class='col col_4 row last'>
+            <p class='big u'>Tinga</p>
+            <p class='big'>Dave: That’s badass! MAN.</p>
+            <p class='big'>Derek. Jesus Christ. That’s super good. It’s spicy. I’m really glad we didn’t get anything to drink.</p>
+        </div>
+
+        <div class='col col_4 row'>
+            <p class='big u'>Cabeza</p>
+            <p class='big'>Dave: It’s super good. That other barbacoa [at El Barzon] was good, but this is the shit. This place rules.</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_pueblo_03.jpg' />
+        </div>
+        
+        <div class='col col_8 row_l'>
+            <img src='/img/content/2013/05/life/tacos_pueblo_04.jpg' />
+        </div>
+        <div class='col col_4 row_l last'>
+            <p class='big u'>Chicharrones</p>
+            <p class='big'>Derek: Shit. Christ. It’s almost sweet.</p>
+            <p class='big'>Dave: We should have gotten ten of these. It’s ten for ten bucks!</p>
+        </div>
+    </div>
+    <div class='callout_row'>
+        <strong>Taqueria Mi Pueblo</strong> 7278 Dix St. Detroit, Michigan 48209
+    </div>
+    </div>
+
+
+    <!-- Primos -->
+    <div id='primos' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>Tacos El Primo</h2>
+        </div>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/05/life/tacos_primo_00.jpg' />
+        </div>
+        <div class='col col_12 c last'>
+            <p class='big order'>The tacos: Carne Asada and Tripa</p>
+        </div>
+
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Carne Asada</p>
+            <p class='big'>Dave: This is real good. But the corn is a tad dry. Get the green sauce.</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_primo_01.jpg' />
+        </div>
+
+        <div class='col col_8 row'>
+            <img src='/img/content/2013/05/life/tacos_primo_02.jpg' />
+        </div>
+        <div class='col col_4 row last'>
+            <p class='big u'>tripa</p>
+            <p class='big'>Dave: DUDE. It’s soft. Holy shit. I’m getting so full.</p>
+        </div>
+
+        <div class='col col_8 row_l'>
+            <img src='/img/content/2013/05/life/tacos_primo_03.jpg' />
+        </div>
+        <div class='col col_4 row_l last'>
+            <p class='big u'>Tripa</p>
+            <p class='big'>Derek: Holy fuck. It’s a super gnarly texture but it’s super good. If I say super one more time, kick me in the nuts.</p>
+        </div>
+    </div>
+    <div class='callout_row'>
+        <strong>Tacos El Primo (Taco Truck)</strong> 312.729.1047
+    </div>
+    </div>
+
+
+    <!-- Lupita's -->
+    <div id='lupitas' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>Taquerita Lupita's</h2>
+        </div>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/05/life/tacos_lupitas_00.jpg' />
+        </div>
+        <div class='col col_12 c last'>
+            <p class='big order'>THE TACOS: Buche (pig stomach), Chicken, and Lomo (pork loin)</p>
+        </div>
+
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Lomo</p>
+            <p class='big'>Dave: REALLY good. But I’m gonna leave the gristle behind. The structural integrity of this taco has been compromised. That’s one fucked up looking taco.</p>
+
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_lupitas_01.jpg' />
+        </div>
+
+        <div class='col col_8 row_l'>
+            <img src='/img/content/2013/05/life/tacos_lupitas_02.jpg' />
+        </div>
+        <div class='col col_4 row_l last'>
+            <p class='big u'>Buche</p>
+            <p class='big'>Derek: You know what it is? Delicious. [About 30 seconds passes.] I regret this… They’re GREAT. But I don’t ever want to eat again… I’m starting to hit my gag reflex…</p>
+            <p class='big'>I hate your website.</p>
+        </div>
+    </div>
+    <div class='callout_row'>
+        <strong>Taqueria Lupita’s</strong> 3443 Bagley St. Detroit, MI
+    </div>
+    </div>
+    
+    
+    <!-- Imperial -->
+    <div id='imperial' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>Imperial</h2>
+        </div>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/05/life/tacos_imperial_00.jpg' />
+        </div>
+        <div class='col col_12 c last'>
+            <p class='big order'>THE ORDER: Vegetarian Taco, Hot Dog, and Swordfish Taco</p>
+        </div>
+
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Vegetarian	</p>
+
+            <p class='big'>Dave: I was feeling a little fat faced but now I’m better. This is the best veggie taco I’ve ever had.</p>
+
+            <p class='big'>Derek: It has the same texture as the tongue one [at Mi Pueblo].</p>
+            <p class='big'>Dave: I’m feeling like I could have a few more all of a sudden.</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_imperial_01.jpg' />
+        </div>
+
+        <div class='col col_8 row'>
+            <img src='/img/content/2013/05/life/tacos_imperial_02.jpg' />
+        </div>
+        <div class='col col_4 row last'>
+            <p class='big u'>Hot Dog</p>
+
+            <p class='big'>Derek: FUCK. That’s super good.</p>
+            <p class='big'>Dave: I’d take half a hot dog over cake any day.</p>
+
+            <p class='big'>Derek: Ten tacos and half a hot dog. I feel like I could do more.</p>
+        </div>
+        
+        <div class='col col_4 row'>
+            <p class='big u'>Swordfish</p>
+
+            <p class='big'>Derek: Why are we eating this?</p>
+            <p class='big'>Dave: This one goes to 11.</p>
+            <p class='big'>Derek: It’s seriously really good. I hate it. Am I hallucinating?</p>
+
+            <p class='big'>Dave: That was really good. I was into it. I never want to eat again.</p>
+        </div>
+        <div class='col col_8 row last'>
+            <img src='/img/content/2013/05/life/tacos_imperial_03.jpg' />
+        </div>
+
+        <div class='col col_8 row_l'>
+            <img src='/img/content/2013/05/life/tacos_imperial_04.jpg' />
+        </div>
+        <div class='col col_4 row_l last'>
+            <p class='big'>Derek: I can’t tell if I’m drunk on tacos or liquor.</p>
+            <p class='big'>Dave: I think there’s so much food in my stomach that I couldn’t get drunk.</p>
+        </div>
+        
+    </div>
+    <div class='callout_row'>
+        <strong>Imperial</strong> 22828 Woodward Ave. Ferndale, MI
+    </div>
+    </div>
+    
+    <div id='tops' class='cols product ac'>
+        <div class='col col_12 c last'>
+            <h2>Top Tacos</h2>
+        </div>
+        <div class='col col_6 col_offset_3 last'>
+            <p class='big'>Derek<br/>
+            Tied for first place: Tripa at Tacos El Primo and Spicy Pork at El Barzon</p>
+
+            <p class='big'>Dave<br/>
+            Tied for first place: Chorizo and Spicy Pork at El Barzon
+            “It was really hard to pick. Also, Lupita’s is amazing but I was so full I could only taste pain.” </p>
+
+        </div>
+        
+        <div class='col col_12 c last'>
+            <h2>Top Restaurants</h2>
+        </div>
+        <div class='col col_6 col_offset_3 last'>
+            <p class='big'>Derek<br/>
+            “I’m gonna go with the taco truck. It was the right place at the right time, mostly because it was conveniently located about 200 feet from our previous stop. The tacos were very matter of fact. They were able to take very simple ingredients and make them go a long way.”</p>
+
+            <p class='big'>Dave<br/>
+            “Mi Pueblo was new to me and a really awesome find. I have been hearing a lot about it lately and was excited to eat there. Ten TACOS for $10!!! I love a deal!</p>
+
+            
+            </p>
+        </div>
+    </div>
+    
+
+
+
+    
+    
+    <?php fin($page['id']); ?>    
+        
+<?php
+    include_once($doc_root . '/includes/footer.php');
+?>
