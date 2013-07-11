2e746c89977d89981b5e7e61b23065451098ea65
jamesv <j@mesv.org>
2013-07-11 09:43:20 -0400

    dcfc

---
+++ 2013/07/life/dcfc.php
@@ -0,0 +1,96 @@
+<?php
+    $page = array(
+        'id' => '2013_07_life_dcfc',
+        'title' => 'Detroit City Football Club',
+        'class' => 'life',
+        'content_css' => true
+    );
+
+    $doc_root = $_SERVER['DOCUMENT_ROOT'];
+    include_once($doc_root . '/includes/header.php'); 
+    include_once($doc_root . '/includes/functions.php'); 
+?>
+    <!-- Containers -->
+    <div class='cols ac intro gutter_top'>
+
+        <div class='title'><h1>Allez<br/>Le Rouge</h1></div>
+        <div class='ac'>
+        <h4 class='ac col col_12 last'>The Detroit City Football Club finally gives devoted soccer fans a home team to support. And they’re giving the team all they’ve got.</h4></div>
+        <div class='credits'>
+            <strong>Photography</strong> Chris Arace
+            <strong>Words</strong> Marguerite Adragna
+        </div> 
+
+    </div>
+    
+
+    <div class='cols product ac'>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/07/life/dcfc_001.jpg' />
+        </div>
+    </div>
+
+
+    
+    <div class='cols product ac'>
+        <div class='col col_12 row last p_cols_2'>
+            <div>
+            <p>“Passion for our city. Passion for the game.” That’s the motto of the Detroit City Football Club, the minor league team that’s turned Cass Tech’s football field into a soccer supporter’s dream. The motto is apt. If pressed to describe the experience of attending a DCFC game in one word, what comes to mind is indeed passion. From the players on the field to the supporters in the stands.</p>
+            <p>Made up of current and former college soccer players, the club, nicknamed “Le Rouge,” has been making quite a bit of noise this summer. As they head into the playoffs, an impressive 11-0-1 record has landed DCFC first place in their division and they’ve been voted into the top spot in the National Premier Soccer League. All the while, their devout supporter groups are busy recruiting new fans and making actual noise. The kind made by cheers, drums, screaming, and singing. Lots. Of. It.</p>
+            </div>
+            <div>
+            <p>When did all this happen? While you were busy eating at Slow’s for the 17th time, five friends and soccer fans decided to form a team that would fill the sport’s void in the city. As soon as word got out, supporter groups formed and got to work to spread the news. It’s a grassroots success story for which any marketer would pay millions. But it can’t possibly be duplicated, because this is born from true passion for the game and the community.</p>
+            <p>“Fans were starved for a team to follow and support here. As soon as DCFC arose, you just saw how big soccer is in the area,” says team captain Josh Rogers.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class='cols product ac'>
+        <div class='col col_8 row'>
+            <img src='/img/content/2013/07/life/dcfc_002.jpg' />
+        </div>
+
+        <div class='col col_4 row last'>
+            <p class='big'>“You’re playing not just for yourself or your teammates, you’re also playing for those fans.” <br/>- DCFC Captain Josh Rogers</p>
+        </div>
+
+    </div>
+
+
+    <div class='cols product ac'>
+        <div class='col col_12 last'>
+            <img src='/img/content/2013/07/life/dcfc_003.jpg' />
+        </div>
+    </div>
+
+    <div class='cols product ac'>
+        <div class='col col_12 row last p_cols_2'>
+        <div>
+        <p>“DCFC seemed, even before there was a roster, to have a lot of passion and a soul,” says Gene Butcher, founding member of the Northern Guard Supporters. “We introduced ourselves to the owners very early on and we could tell that there was indeed a lot of hard work and grassroots passion already in the team, just from the owners. It was a really good match for what we wanted to do, which was a 100% all-in Supporter Group for one club and not general support for the entire region and all of the teams in it. We only support DCFC.” </p>
+        <p>Their undying devotion simply can’t be ignored. Whether they’re leading the crowd in songs or cheers, waving enormous hand-painted flags or lighting smoke tubes, the Northern Guard makes sure the supporter section (located in the away stands) is a party. And they encourage everyone to join in. “We go out of our way to welcome new people,” says Butcher. “All we need is a few minutes and we can have you hooked and singing like a lunatic before you know it.”</p>
+        </div>
+        
+        <div>
+        <p>It’s grown into an experience the likes of which this city, and possibly the league, has never seen. “With those guys cheering, chanting, screaming and lighting off flares, it gives you that extra boost. You’re playing not just for yourself or your teammates, you’re also playing for those fans,” says Rogers.</p>
+        <p>“Playing for these fans is just incredible,” adds central mid-fielder Latif Alashe. “They bring so much energy to every game and make playing a Cass Tech such an interesting and fun experience.”</p>
+        <p>Between the hard work of supporter groups, word of mouth from casual fans and, most recently, a lot of local media coverage, the news has spread. So much so that each home game brings a new, record-breaking crowd. “I think a lot of hard work is paying off,” says Butcher. “We have literally busted our butts to bring attention to DCFC. We want the news to travel far and wide that this is the hottest club in the Midwest, and if you miss out you will regret it.”</p>
+        <p>You heard the man. Playoffs begin Saturday, July 13 at Cass Tech. Be there or miss out.</p>
+        <p><em>For tickets and game day info, head to Detroitcityfc.com.</em></p>
+        </div>
+        </div>
+    </div>
+    
+    
+    
+        
+</div>
+
+
+
+    
+    <?php fin($page['id']); ?>    
+        
+<?php
+    include_once($doc_root . '/includes/footer.php');
+?>
